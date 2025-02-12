5270c8b47a2499ed91b901c70e5b1dbb0c2a2748
newgenai <newgenai79@gmail.com>
2025-02-13 00:05:02 +0530

    add Lumina Image 1.0 and 2.0

--- app.py
+++ app.py
@@ -14,73 +14,28 @@ sys.path.append(tabs_dir)
 """
 
 # Text 2 Image
-from modules.text2image.tab_sana import create_sana_tab
-from modules.text2image.tab_flex1_alpha_gguf import create_flex1_alpha_gguf_tab
 from modules.text2image.tab_lumina import create_lumina_tab
 from modules.text2image.tab_lumina2_bnb import create_lumina2_BnB_tab
-# from modules.text2image.tab_lumina2_gguf import create_lumina2_gguf_tab
-from modules.text2image.tab_cogview3plus import create_cogView3Plus_tab
-from modules.text2image.tab_hunyuandit import create_hunyuandit_tab
-from modules.text2image.tab_kandinsky3 import create_kandinsky3_tab
-from modules.text2image.tab_auraflow_gguf import create_auraflow_gguf_tab
-
-# Text 2 Video
-from modules.text2video.tab_ltxvideo091 import create_ltxvideo091_tab
-from modules.text2video.tab_hunyuanvideo_gguf import create_hunyuanvideo_gguf_tab
-from modules.text2video.tab_hunyuanvideo_bnb import create_hunyuanvideo_bnb_tab
-from modules.text2video.tab_cogvideox155b import create_cogvideox155b_t2v_tab
-
-# Image 2 Video
-from modules.image2video.tab_ltximage2video091 import create_ltximage2video091_tab
-from modules.image2video.tab_cogvideox155b_i2v import create_cogvideox155b_i2v_tab
-
-# Video 2 Video
-from modules.video2video.tab_cogvideox155b_f2v import create_cogvideox155b_f2v_tab
-from modules.video2video.tab_cogvideox155b_v2v import create_cogvideox155b_v2v_tab
+from modules.text2image.tab_lumina2_gguf import create_lumina2_gguf_tab
 
 with gr.Blocks() as dwebui:
     gr.Markdown("# WebUI for Image/Video generation")
     with gr.Tabs():
         with gr.Tab("Text 2 Image"):
             with gr.Tabs():
-                with gr.Tab("Sana"):
-                    create_sana_tab()
                 with gr.Tab("Lumina Image 2.0 - BnB"):
                     create_lumina2_BnB_tab()
-                # with gr.Tab("Lumina Image 2.0 - GGUF"):
-                    # create_lumina2_gguf_tab()
-                with gr.Tab("Flex.1-alpha-GGUF"):
-                    create_flex1_alpha_gguf_tab()
-                with gr.Tab("Lumina Next SFT"):
+                with gr.Tab("Lumina Image 2.0 - GGUF"):
+                    create_lumina2_gguf_tab()
+                with gr.Tab("Lumina 1.0 Next SFT"):
                     create_lumina_tab()
-                with gr.Tab("CogView3 Plus"):
-                    create_cogView3Plus_tab()
-                with gr.Tab("Hunyuan DiT"):
-                    create_hunyuandit_tab()
-                with gr.Tab("Kandinsky-3"):
-                    create_kandinsky3_tab()
-                with gr.Tab("AuraFlow 0.3-GGUF"):
-                    create_auraflow_gguf_tab()
         with gr.Tab("Text 2 Video"):
-            with gr.Tabs():
-                with gr.Tab("LTX-Video 0.9.1"):
-                    create_ltxvideo091_tab()
-                with gr.Tab("HunyuanVideo-GGUF"):
-                    create_hunyuanvideo_gguf_tab()
-                with gr.Tab("HunyuanVideo-BitsnBytes"):
-                    create_hunyuanvideo_bnb_tab()
-                with gr.Tab("CogVideoX v1.5-5b"):
-                    create_cogvideox155b_t2v_tab()
+            print("")
         with gr.Tab("Image 2 Video"):
-            with gr.Tabs():
-                with gr.Tab("LTX-Video 0.9.1"):
-                    create_ltximage2video091_tab()
-                with gr.Tab("CogVideoX v1.5-5b"):
-                    create_cogvideox155b_i2v_tab()
+            print("")
         with gr.Tab("Video 2 Video"):
-                with gr.Tab("CogVideoX-Fun v1.1-5b-Pose"):
-                    create_cogvideox155b_f2v_tab()
-                with gr.Tab("CogVideoX v1.5-5b"):
-                    create_cogvideox155b_v2v_tab()
+            print("")
+        with gr.Tab("Extra"):
+            print("")
 
 dwebui.launch(share=False)
---
+++ modules/text2image/tab_lumina2_bnb.py
@@ -0,0 +1,268 @@
+"""
+Copyright NewGenAI
+Do not remove this copyright. No derivative code allowed.
+"""
+import torch
+import gradio as gr
+import numpy as np
+import os
+import modules.util.appstate
+from datetime import datetime
+from diffusers import Lumina2Text2ImgPipeline
+from modules.util.utilities import clear_previous_model_memory
+from modules.util.appstate import state_manager
+from diffusers import BitsAndBytesConfig
+
+MAX_SEED = np.iinfo(np.int32).max
+OUTPUT_DIR = "output/t2i/Lumina"
+
+def random_seed():
+    return torch.randint(0, MAX_SEED, (1,)).item()
+def get_dimensions(resolution):
+    width, height = map(int, resolution.split('x'))
+    return width, height
+def generate_crop_size_list(num_patches, patch_size, max_ratio=4.0):
+    assert max_ratio >= 1.0
+    crop_size_list = []
+    wp, hp = num_patches, 1
+    while wp > 0:
+        if max(wp, hp) / min(wp, hp) <= max_ratio:
+            if ((wp * patch_size)//32) % 2 == 0 and  ((hp * patch_size)//32) % 2 == 0:
+                crop_size_list.append((wp * patch_size, hp * patch_size))
+        if (hp + 1) * wp <= num_patches:
+            hp += 1
+        else:
+            wp -= 1
+    return crop_size_list
+def get_pipeline(quantization, memory_optimization, vaeslicing, vaetiling):
+    print("----Lumina mode: ", quantization, memory_optimization, vaeslicing, vaetiling)
+    # If model is already loaded with same configuration, reuse it
+    if (modules.util.appstate.global_pipe is not None and 
+        type(modules.util.appstate.global_pipe).__name__ == "Lumina2Text2ImgPipeline" and
+        modules.util.appstate.global_quantization == quantization and 
+        modules.util.appstate.global_memory_mode == memory_optimization):
+        print(">>>>Reusing Lumina2 pipe<<<<")
+        return modules.util.appstate.global_pipe
+    else:
+        clear_previous_model_memory()
+
+    bfl_repo = "Alpha-VLLM/Lumina-Image-2.0"
+    dtype = torch.bfloat16
+    if (quantization == "None"):
+        modules.util.appstate.global_pipe = Lumina2Text2ImgPipeline.from_pretrained(
+            bfl_repo,
+            torch_dtype=dtype,
+        )
+    elif (quantization == "int8"):
+        from diffusers import Lumina2Transformer2DModel
+        quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+        transformer_8bit = Lumina2Transformer2DModel.from_pretrained(
+            bfl_repo,
+            subfolder="transformer",
+            quantization_config=quantization_config,
+            torch_dtype=dtype,
+        )
+        modules.util.appstate.global_pipe = Lumina2Text2ImgPipeline.from_pretrained(
+            bfl_repo,
+            transformer=transformer_8bit,
+            torch_dtype=dtype
+        )
+    elif (quantization == "int4"):
+        from diffusers import Lumina2Transformer2DModel
+        quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=dtype)
+        transformer_4bit = Lumina2Transformer2DModel.from_pretrained(
+            bfl_repo,
+            subfolder="transformer",
+            quantization_config=quantization_config,
+            torch_dtype=dtype,
+        )
+        modules.util.appstate.global_pipe = Lumina2Text2ImgPipeline.from_pretrained(
+            bfl_repo,
+            transformer=transformer_4bit,
+            torch_dtype=torch.bfloat16
+        )
+        
+    if memory_optimization == "Low VRAM":
+        modules.util.appstate.global_pipe.enable_model_cpu_offload()
+
+    if vaeslicing:
+        modules.util.appstate.global_pipe.vae.enable_slicing()
+    else:
+        modules.util.appstate.global_pipe.vae.disable_slicing()
+    if vaetiling:
+        modules.util.appstate.global_pipe.vae.enable_tiling()
+    else:
+        modules.util.appstate.global_pipe.vae.disable_tiling()
+        
+    # Update global variables
+    modules.util.appstate.global_memory_mode = memory_optimization
+    modules.util.appstate.global_quantization = quantization
+    return modules.util.appstate.global_pipe
+
+def generate_images(
+    seed, prompt, negative_prompt, resolution, guidance_scale,
+    num_inference_steps, memory_optimization, vaeslicing, vaetiling, quantization
+):
+    if modules.util.appstate.global_inference_in_progress == True:
+        print(">>>>Inference in progress, can't continue<<<<")
+        return None
+    modules.util.appstate.global_inference_in_progress = True
+    try:
+        width, height = get_dimensions(resolution)
+        # Get pipeline (either cached or newly loaded)
+        pipe = get_pipeline(quantization, memory_optimization, vaeslicing, vaetiling)
+        generator = torch.Generator(device="cpu").manual_seed(seed)
+
+        progress_bar = gr.Progress(track_tqdm=True)
+        def callback_on_step_end(pipe, i, t, callback_kwargs):
+            progress_bar(i / num_inference_steps, desc=f"Generating image (Step {i}/{num_inference_steps})")
+            return callback_kwargs
+
+        # Prepare inference parameters
+        inference_params = {
+            "prompt": prompt,
+            "height": height,
+            "width": width,
+            "guidance_scale": guidance_scale,
+            "num_inference_steps": num_inference_steps,
+            "generator": generator,
+            "callback_on_step_end": callback_on_step_end,
+        }
+
+        # Generate images
+        image = pipe(**inference_params).images[0]
+
+        # Create output directory if it doesn't exist
+        os.makedirs(OUTPUT_DIR, exist_ok=True)
+        
+        base_filename = "lumina2.png"
+        
+        gallery_items = []
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+        filename = f"{timestamp}_{base_filename}"
+        output_path = os.path.join(OUTPUT_DIR, filename)
+        
+        # Save the image
+        image.save(output_path)
+        print(f"Image generated: {output_path}")
+        modules.util.appstate.global_inference_in_progress = False
+        # Add to gallery items
+        gallery_items.append((output_path, "Lumina 2"))
+        
+        return gallery_items
+    except Exception as e:
+        print(f"Error during inference: {str(e)}")
+        return None
+    finally:
+        modules.util.appstate.global_inference_in_progress = False
+
+def create_lumina2_BnB_tab():
+    initial_state = state_manager.get_state("lumina2") or {}
+    with gr.Row():
+        with gr.Column():
+            lumina2_quantization = gr.Radio(
+                choices=["None", "int4", "int8"],
+                label="BitsnBytes quantization",
+                value=initial_state.get("quantization", "None"),
+                interactive=True
+            )
+        with gr.Column():
+            lumina2_memory_optimization = gr.Radio(
+                choices=["No optimization", "Low VRAM"],
+                label="Memory Optimization",
+                value=initial_state.get("memory_optimization", "Low VRAM"),
+                interactive=True
+            )
+        with gr.Column():
+            lumina2_vaeslicing = gr.Checkbox(label="VAE Slicing", value=initial_state.get("vaeslicing", True), interactive=True)
+            lumina2_vaetiling = gr.Checkbox(label="VAE Tiling", value=initial_state.get("vaetiling", True), interactive=True)
+    with gr.Row():
+        with gr.Column():
+            lumina2_prompt_input = gr.Textbox(
+                label="Prompt", 
+                lines=6,
+                interactive=True
+            )
+            lumina2_negative_prompt_input = gr.Textbox(
+                label="Negative Prompt",
+                lines=3,
+                interactive=True
+            )
+        with gr.Column():
+            with gr.Row():
+                lumina2_resolution_dropdown = gr.Dropdown(
+                    choices=[f"{w}x{h}" for w, h in generate_crop_size_list((1024 // 64) ** 2, 64)],
+                    value=initial_state.get("resolution", "1024x1024"),
+                    label="Resolution"
+                )
+                seed_input = gr.Number(label="Seed", value=0, minimum=0, maximum=MAX_SEED, interactive=True)
+                dummy = gr.Textbox(
+                    label="", 
+                    visible=False
+                )
+                random_button = gr.Button("♻️ Randomize seed")
+            with gr.Row():
+                lumina2_guidance_scale_slider = gr.Slider(
+                    label="Guidance Scale", 
+                    minimum=1.0, 
+                    maximum=20.0, 
+                    value=initial_state.get("guidance_scale", 4.0),
+                    step=0.1,
+                    interactive=True
+                )
+                lumina2_num_inference_steps_input = gr.Number(
+                    label="Number of Inference Steps", 
+                    value=initial_state.get("inference_steps", 40),
+                    interactive=True
+                )
+            with gr.Row():
+                save_state_button = gr.Button("💾 Save State")
+    with gr.Row():
+        generate_button = gr.Button("🎨 Generate image")
+    output_gallery = gr.Gallery(
+        label="Generated Image(s)",
+        columns=3,
+        rows=None,
+        height="auto"
+    )
+
+    def save_current_state(quantization, memory_optimization, vaeslicing, vaetiling, resolution, guidance_scale, inference_steps):
+        state_dict = {
+            "quantization": quantization,
+            "memory_optimization": memory_optimization,
+            "vaeslicing": vaeslicing,
+            "vaetiling": vaetiling,
+            "resolution": resolution,
+            "guidance_scale": guidance_scale,
+            "inference_steps": inference_steps
+        }
+        # print("Saving state:", state_dict)
+        initial_state = state_manager.get_state("lumina2") or {}
+        return state_manager.save_state("lumina2", state_dict)
+
+    # Event handlers
+    random_button.click(fn=random_seed, outputs=[seed_input])
+    save_state_button.click(
+        fn=save_current_state,
+        inputs=[
+            lumina2_quantization,
+            lumina2_memory_optimization, 
+            lumina2_vaeslicing, 
+            lumina2_vaetiling, 
+            lumina2_resolution_dropdown, 
+            lumina2_guidance_scale_slider, 
+            lumina2_num_inference_steps_input
+        ],
+        outputs=[gr.Textbox(visible=False)]
+    )
+
+    generate_button.click(
+        fn=generate_images,
+        inputs=[
+            seed_input, lumina2_prompt_input, lumina2_negative_prompt_input, 
+            lumina2_resolution_dropdown, lumina2_guidance_scale_slider, 
+            lumina2_num_inference_steps_input, lumina2_memory_optimization, 
+            lumina2_vaeslicing, lumina2_vaetiling, lumina2_quantization
+        ],
+        outputs=[output_gallery]
+    )
